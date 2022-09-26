8837cee9318832d0757abb3ad530d6ad39e1ed26
inhegass <112832963+inhegass@users.noreply.github.com>
2022-09-26 00:14:04 -0300

    Add files via upload

---
+++ JogoDaForca(C2).c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+//biblioteca para pegar o strlen
+
+int main(){
+	char palavrasecreta[20];
+	//isso é um arrays que guarda muitas coisas
+	sprintf(palavrasecreta, "MELANCIA");
+	//sprintf imprime em um array
+	//ultimo caracter do array com spritf é sempre /0, por criar um loop, mas isso n muda o array em sí
+	
+	int acertou = 0;
+	int enforcou = 0 ;
+	
+	char chutes[26];
+	int tentativas = 0;
+		
+	do {		
+		for (int i = 0; i < strlen(palavrasecreta); i++){
+			//strlen mostra o tamanho do array ou string
+			int achou = 0;
+			for(int j = 0; j < tentativas; j++){
+				if(chutes[j] == palavrasecreta[i]){
+					achou = 1;
+					break;
+				}
+			}
+			if(achou){
+				printf("%c ", palavrasecreta[i]);
+			}
+			else{
+				printf("_ ");
+			}
+		}
+		printf("\n");
+		
+		char chute;
+		printf("Qual letra?\n");
+		scanf(" %c", &chute);
+		// coloca o espaço para ignorar o enter e não considera-lo como char
+		
+		chutes[tentativas] = chute;
+		tentativas ++;		
+	} while (!acertou && !enforcou);
+	//executa uma vez mesmo que o programa ja tenha a função falseada
+	//a "!" serve como negação (0)
+	//"&&" = "e"
+}
