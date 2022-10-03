a2499fb03f7d67972dee4a6b3031491739630c51
inhegass <112832963+inhegass@users.noreply.github.com>
2022-10-03 06:27:49 -0300

    Add files via upload

---
+++ forca.h
@@ -0,0 +1,12 @@
+#define TAMANHO_PALAVRA 20
+//constante
+
+void abertura();
+//declaro que uma função existe para o computador para que, se eu usar uma função dentro de uma função que foi escrita depois, o computador procura ela no arquivo para usar dentro da função
+void chuta(int* chutesdados);
+void desenhaforca(int chutesdados);
+void escolhepalavra();
+int acertou(int chutesdados);
+int jachutou(char letra, int chutesdados);
+void adicionapalvra();
+void enforcou(int chutesdados);
